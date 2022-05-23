edca0ad4a356e5c4034bd0bb217799503e5769bd
Mikhail Fursa <bellamy9678@yahoo.com>
2022-05-24 00:18:58 +0200

    feat: remove unused components and packages

--- src/components/Title.tsx
+++ src/components/Title.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { Heading } from "@chakra-ui/layout";
 import { translations } from "../utils";
 
-export const FunctionalTitle = () => {
+export const Title = () => {
   return (
     <Heading as="h2" size="3xl" noOfLines={1}>
         {`${translations.en.prefix.func}: ${translations.en.title}`}
