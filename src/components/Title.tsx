f5ad3ec03208fc6f4859c9513def264fb4010f0d
Mikhail Fursa <bellamy9678@yahoo.com>
2022-06-12 19:47:29 +0200

    refactor: move en json to translations

--- src/components/Title.tsx
+++ src/components/Title.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Heading } from "@chakra-ui/layout";
-import { translations } from "../utils";
+import { translations } from "../translations";
 
 export const Title = () => {
   return (
